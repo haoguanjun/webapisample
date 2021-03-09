51c8a1908d5b24cbb5afed5be41a1109581f81bd
haoguanjun <haoguanjun@gmail.com>
2021-03-09 22:58:50 +0800

    Add files via upload

---
+++ ApiControllerBase.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace lovepdf.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiControllerBase : ControllerBase
+    {
+    }
+}
